e7698045b2dfd070c90fb1a61a4c4a3ce2aab65f
zidanewenqsh <zidanewenqsh@163.com>
2023-10-25 19:19:06 +0800

    20231025-2

--- Plugins/plugin_demo.py
+++ Plugins/plugin_demo.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import semantic_kernel as sk
+import os
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 kernel = sk.Kernel()
-# functions = kernel.import_semantic_skill_from_directory(
-#     "./sk_samples/", "SamplePlugin"
-# )
-# cli = functions["GenerateCommand"]
-
-# 加载 semantic function。注意目录结构
+api_key = os.getenv("OPENAI_API_KEY")
+endpoint = os.getenv("OPENAI_API_BASE")
+model = OpenAIChatCompletion("gpt-3.5-turbo", api_key, endpoint=endpoint)
+# 安装驱动程序
+kernel.add_text_completion_service("my-gpt3", model)
 functions = kernel.import_semantic_skill_from_directory(
-    "./sk_samples/", "SamplePlugin")
+    "./sk_samples/", "SamplePlugin"
+)
 cli = functions["GenerateCommand"]
-
-# 看结果
 print(cli("将系统日期设为2023-04-01"))
